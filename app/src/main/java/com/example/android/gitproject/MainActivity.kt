b8643fea224ec86df55367f9de85082aeee24d61
Arvind Kumar <arvindkumar72446@gmail.com>
2022-08-02 12:54:25 +0530

    Third Commit

--- app/src/main/java/com/example/android/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/android/gitproject/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.android.gitproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This line is include in my second commit")
+
+        println("This is in third commit and should not be included we need to revert it")
     }
 }
