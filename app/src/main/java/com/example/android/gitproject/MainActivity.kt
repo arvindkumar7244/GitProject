d48600074e698f084b4c396611d968e2425257df
Arvind Kumar <arvindkumar72446@gmail.com>
2022-08-02 13:27:15 +0530

    Experimental Commit

--- app/src/main/java/com/example/android/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/android/gitproject/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.android.gitproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This line is include in my second commit")
+
+        println("Local Change")
+
+        println("Experimental")
     }
 }
